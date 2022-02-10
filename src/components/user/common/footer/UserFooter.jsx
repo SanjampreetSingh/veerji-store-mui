30531ea26c119c8ca4a38ac7579566e7132b83fb
Sanjampreet Singh <sanjam531@gmail.com>
2022-02-10 15:33:46 +0530

    added footer

---
+++ src/components/user/common/footer/UserFooter.jsx
@@ -0,0 +1,41 @@
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+
+export default function UserFooter() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+        py: 3,
+        px: 2,
+        mt: "auto",
+        backgroundColor: (theme) => theme.palette.grey[100],
+      }}
+    >
+      <Grid container justifyContent="center">
+        <Grid item xs={12} sm={4}>
+          <Typography variant="h6" color="text.secondary">
+            {"© " + new Date().getFullYear()} &nbsp;
+            <Link
+              color="inherit"
+              href="https://veerji.store/"
+              underline="hover"
+            >
+              Veerji Departmental Store, Ldh
+            </Link>
+          </Typography>
+        </Grid>
+        <Grid item xs={12} sm={2}>
+          <Link href="https://veerji.store/">
+            <ShoppingBasketIcon />
+          </Link>
+        </Grid>
+        <Grid item xs={12} sm={2} />
+      </Grid>
+    </Box>
+  );
+}
--- src/layouts/home/HomeLayout.jsx
+++ src/layouts/home/HomeLayout.jsx
@@ -1,11 +1,26 @@
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import UserHeader from "../../components/user/common/header/UserHeader";
+import UserFooter from "../../components/user/common/footer/UserFooter";
 
 export default function HomeLayout(props) {
-  const { isAuthenticated, children } = props
+  const { isAuthenticated, children } = props;
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+      }}
+    >
+      <CssBaseline />
       <UserHeader isAuthenticated={isAuthenticated} />
-      <div className="px-4 py-5 mb-5 ">{children}</div>
-    </>
+      <Container component="main" maxWidth="lg">
+        {children}
+      </Container>
+      <UserFooter />
+    </Box>
   );
 }
