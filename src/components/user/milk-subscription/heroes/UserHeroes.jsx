1524e075996f652a341160f96f23e2b0b2174d5c
Sanjampreet Singh <sanjam531@gmail.com>
2022-03-10 00:48:54 +0530

    get user details ui made

--- src/components/user/milk-subscription/heroes/UserHeroes.jsx
+++ src/components/user/milk-subscription/heroes/UserHeroes.jsx
@@ -1,6 +1,12 @@
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import PersonIcon from "@mui/icons-material/Person";
+
+import { Link } from "react-router-dom";
 
 export default function UserHeroes(props) {
   const { user } = props;
@@ -19,25 +25,78 @@ export default function UserHeroes(props) {
           color="primary"
           fontSize="inherit"
           sx={{
-            fontSize: "80px",
+            fontSize: "60px",
             border: "2px solid",
             borderRadius: "50%",
             borderMargin: "5px",
           }}
         />
       </Typography>
-      <Typography
-        component="h4"
-        variant="h2"
-        align="center"
-        color="text.primary"
-        gutterBottom
-      >
-        Veerji Departmental Store
-      </Typography>
+
       <Typography variant="h5" align="center" color="text.secondary" paragraph>
         Hi <em>{user?.name}</em>, now pay for your milk subscription online.
       </Typography>
+      <Typography align="center">
+        <Stack
+          spacing={2}
+          direction={{ xs: "column", sm: "row" }}
+          justifyContent="center"
+        >
+          <Grid item component={Paper} variant="outlined" sx={{ p: 1 }}>
+            <Typography variant="subtitle1">
+              Contact:{" "}
+              <a
+                href={"tel:+91-" + user?.phone}
+                target="_blank"
+                rel="noreferrer"
+              >
+                +91-{user?.phone}
+              </a>
+            </Typography>
+          </Grid>
+          <Grid item component={Paper} variant="outlined" sx={{ p: 1 }}>
+            {" "}
+            <Typography variant="subtitle1">
+              Email:{" "}
+              <a
+                href={"mailto:" + user?.email}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {user?.email}
+              </a>
+            </Typography>
+          </Grid>
+          <Grid item component={Paper} variant="outlined" sx={{ p: 1 }}>
+            <Typography variant="subtitle1">
+              Address:{" "}
+              <a
+                href={
+                  "http://maps.google.com/?q=:" +
+                  user?.house_number +
+                  " " +
+                  user?.locality_name
+                }
+                target="_blank"
+                rel="noreferrer"
+              >
+                {user?.house_number + " " + user?.locality_name}
+              </a>
+            </Typography>
+          </Grid>
+        </Stack>
+      </Typography>
+
+      <Stack sx={{ pt: 2 }} direction="row" spacing={2} justifyContent="center">
+        <Grid item component={Paper} elevation={3} sx={{ p: 1 }}>
+          <Typography variant="subtitle1">
+            Payment: ₹{" " + user?.payment}
+          </Typography>
+        </Grid>
+        <Button variant="contained" component={Link} to="/milk-subscription">
+          Make Payment
+        </Button>
+      </Stack>
     </Box>
   );
 }
